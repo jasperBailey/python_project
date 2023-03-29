71b214fab92bf27becb0c6c4de42d3851e3007e3
user <user@users-MacBook-Pro.local>
2023-03-29 12:25:48 +0100

    redid CSS and added notes index

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from controllers.tracks_controller import tracks_blueprint
+from controllers.note_controller import notes_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(tracks_blueprint)
+app.register_blueprint(notes_blueprint)
 
 @app.route('/')
 def home():
--- controllers/note_controller.py
+++ controllers/note_controller.py
@@ -1,54 +1,49 @@
 from flask import render_template, request, redirect, Blueprint
 from repositories import note_repository
 from repositories import track_repository
-from models.note import Note
 
 notes_blueprint = Blueprint("notes", __name__)
 
-@notes_blueprint.route("/notes")
-def notes():
-    notes = note_repository.select_all()
-    return render_template("notes/index.html", all_notes = notes)
-
-@notes_blueprint.route('/notes/new')
-def new_note():
-    tracks = track_repository.select_all()
-    return render_template("notes/new.html", all_tracks = tracks)
-
-@notes_blueprint.route('/notes', methods=['POST'])
-def create_note():
-    pitch = request.form['pitch']
-    track_id = request.form['track_id']
-    duration = request.form['duration']
-    location = request.form['location']
+@notes_blueprint.route("/tracks/<track_id>/notes")
+def notes(track_id):
     track = track_repository.select(track_id)
-    note = Note(pitch, track, duration, location)
-    note_repository.save(note)
-    return redirect('/notes')
+    notes = note_repository.notes_in_track(track)
+    return render_template("notes/index.html", all_notes=notes, track=track)
 
-@notes_blueprint.route('/notes/<index>')
-def show_note(index):
-    note = note_repository.select(index)
-    return render_template('notes/show.html', note=note)
+# @notes_blueprint.route('/tracks/<track_id>/notes/new')
+# def new_note():
+#     tracks = track_repository.select_all()
+#     return render_template("notes/new.html", all_tracks = tracks)
 
-@notes_blueprint.route('/notes/<index>/edit')
-def edit_note(index):
-    note = note_repository.select(index)
-    tracks = track_repository.select_all()
-    return render_template('notes/edit.html', note=note, all_tracks=tracks)
+# @notes_blueprint.route('/tracks/<track_id>/notes', methods=['POST'])
+# def create_note():
+#     pitch = request.form['pitch']
+#     track_id = request.form['track_id']
+#     duration = request.form['duration']
+#     location = request.form['location']
+#     track = track_repository.select(track_id)
+#     note = Note(pitch, track, duration, location)
+#     note_repository.save(note)
+#     return redirect('/notes')
 
-@notes_blueprint.route('/notes/<index>', methods=['POST'])
-def update_note(index):
-    pitch = request.form['pitch']
-    track_id = request.form['track_id']
-    duration = request.form['duration']
-    location = request.form['location']
-    track = track_repository.select(track_id)
-    note = Note(pitch, track, duration, location, index)
-    note_repository.update(note)
-    return redirect('/notes')
+# @notes_blueprint.route('/tracks/<track_id>/notes/<note_id>/edit')
+# def edit_note(note_id):
+#     note = note_repository.select(note_id)
+#     tracks = track_repository.select_all()
+#     return render_template('notes/edit.html', note=note, all_tracks=tracks)
 
-@notes_blueprint.route('/notes/<index>/delete', methods=['POST'])
-def delete_note(index):
-    note_repository.delete(index)
-    return redirect('/notes')
+# @notes_blueprint.route('/tracks/<track_id>/notes/<note_id>', methods=['POST'])
+# def update_note(note_id):
+#     pitch = request.form['pitch']
+#     track_id = request.form['track_id']
+#     duration = request.form['duration']
+#     location = request.form['location']
+#     track = track_repository.select(track_id)
+#     note = Note(pitch, track, duration, location, note_id)
+#     note_repository.update(note)
+#     return redirect('/notes')
+
+# @notes_blueprint.route('/tracks/<track_id>/notes/<note_id>/delete', methods=['POST'])
+# def delete_note(note_id):
+#     note_repository.delete(note_id)
+#     return redirect('/notes')
